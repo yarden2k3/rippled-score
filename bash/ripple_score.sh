e150d3fc59aa687e31e15989065921751cc94467
Yarden <yarden2k3@gmail.com>
2018-07-31 21:13:34 +0300

    SEQUENCE fixed!

--- bash/ripple_score.sh
+++ bash/ripple_score.sh
@@ -1,17 +1,18 @@
 #! /bin/bash
 
 function create_player {
+	BANKSTAT=`user_status rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh`
+	SEQUENCE=`echo $BANKSTAT |  grep "Sequence\"\s*:\s*[0-9]*" -o | sed 's/Sequence" : //'`
 	WALLET=`rippled wallet_propose 2>/dev/null`
 	ACCOUNT=`echo $WALLET |  grep "account_id\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/account_id" : "//'`
 	SECRET=`echo $WALLET |  grep "master_seed\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/master_seed" : "//'`
 	FEE=10
-	SEQUENCE=2
-	TRANSACTION='{"TransactionType": "Payment", "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "Destination": "'$ACCOUNT'", "Amount": "300000020",  "Fee": "'$FEE'", "Flags": "0", "Sequence": "'$SEQUENCE'"}'
+	TRANSACTION='{"TransactionType": "Payment", "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "Destination": "'$ACCOUNT'", "Amount": "301000000",  "Fee": "'$FEE'", "Flags": "0", "Sequence": "'$SEQUENCE'"}'
 	SIGN=`rippled sign "snoPBrXtMeMyMHUVTgbuqAfg1SUTb" "$TRANSACTION" offline 2>/dev/null`
 	TXBLOB=`echo $SIGN | grep "tx_blob\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/tx_blob" : "//'`
 	rippled submit $TXBLOB 2>/dev/null >/dev/null
 
-
+	SEQUENCE=1
 	REGWALLET=`rippled wallet_propose 2>/dev/null`
 	REGACC=`echo $REGWALLET |  grep "account_id\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/account_id" : "//'`
 	TRANSACTION='{"TransactionType": "SetRegularKey", "Account": "'$ACCOUNT'", "RegularKey": "'$REGACC'",  "Fee": "'$FEE'", "Sequence": "'$SEQUENCE'"}'
@@ -27,8 +28,9 @@ function transfer_points {
 	SCNDPLAYER=$2
 	SECRET=$3
 	
+	FPSEQ=`user_status $FRSTPLAYER`
 	FEE=10
-	SEQUENCE=3
+	SEQUENCE=`echo $FPSEQ |  grep "Sequence\"\s*:\s*[0-9]*" -o | sed 's/Sequence" : //'`
 	TRANSACTION='{"TransactionType": "Payment", "Account": "'$FRSTPLAYER'", "Destination": "'$SCNDPLAYER'", "Amount": "5000000",  "Fee": "'$FEE'", "Flags": "0", "Sequence": "'$SEQUENCE'"}'
 	SIGN=`rippled sign $SECRET "$TRANSACTION" offline 2>/dev/null`
 	TXBLOB=`echo $SIGN | grep "tx_blob\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/tx_blob" : "//'`
