6f6b663ab2d7b6ee2da969d733b0a6a4946a7be5
Yarden <yarden2k3@gmail.com>
2018-07-31 17:38:00 +0300

    the function top_players has added

--- bash/ripple_score.sh
+++ bash/ripple_score.sh
@@ -5,8 +5,8 @@ function create_player {
 	ACCOUNT=`echo $WALLET |  grep "account_id\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/account_id" : "//'`
 	SECRET=`echo $WALLET |  grep "master_seed\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/master_seed" : "//'`
 	FEE=10
-	SEQUENCE=1
-	TRANSACTION='{"TransactionType": "Payment", "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "Destination": "'$ACCOUNT'", "Amount": "300000010",  "Fee": "'$FEE'", "Flags": "0", "Sequence": "'$SEQUENCE'"}'
+	SEQUENCE=2
+	TRANSACTION='{"TransactionType": "Payment", "Account": "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh", "Destination": "'$ACCOUNT'", "Amount": "300000020",  "Fee": "'$FEE'", "Flags": "0", "Sequence": "'$SEQUENCE'"}'
 	SIGN=`rippled sign "snoPBrXtMeMyMHUVTgbuqAfg1SUTb" "$TRANSACTION" offline 2>/dev/null`
 	TXBLOB=`echo $SIGN | grep "tx_blob\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/tx_blob" : "//'`
 	rippled submit $TXBLOB 2>/dev/null >/dev/null
@@ -28,9 +28,61 @@ function transfer_points {
 	SECRET=$3
 	
 	FEE=10
-	SEQUENCE=2
+	SEQUENCE=3
 	TRANSACTION='{"TransactionType": "Payment", "Account": "'$FRSTPLAYER'", "Destination": "'$SCNDPLAYER'", "Amount": "5000000",  "Fee": "'$FEE'", "Flags": "0", "Sequence": "'$SEQUENCE'"}'
 	SIGN=`rippled sign $SECRET "$TRANSACTION" offline 2>/dev/null`
 	TXBLOB=`echo $SIGN | grep "tx_blob\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/tx_blob" : "//'`
 	rippled submit $TXBLOB 2>/dev/null >/dev/null
 }
+
+function user_status {
+	echo `rippled account_info $1 2>/dev/null`
+}
+
+function top_ten {
+	NAME=$1[@]
+	ACCARR=("${!NAME}")
+	declare -A HASHACC
+	declare -A HASHMAXACC
+#	for ACC in "${ACCARR4[@]}"; do echo "$ACC"; done
+
+	for ACC in "${ACCARR[@]}"
+	do
+		USER=`user_status $ACC`
+		BALANCE=`echo $USER | grep "Balance\"\s*:\s*\"[0-9]*" -o | sed 's/Balance" : "//'`
+		HASHACC["$ACC"]="$BALANCE"
+	done
+
+#	for ACC in "${!HASHACC[@]}"; do echo "$ACC - ${HASHACC[$ACC]}"; done
+
+	MAX=0
+	COUNTER=1
+	if [ ${#ACCARR[@]} -ge 10 ]
+	then
+		PLAYERSNUM=10
+	else
+		PLAYERSNUM=${#ACCARR[@]}
+	fi
+	
+	while [ $COUNTER -le $PLAYERSNUM ]; do
+		for ACC in "${!HASHACC[@]}"; do
+			POTENTIAL=${HASHACC[$ACC]}
+			if [ $POTENTIAL -gt $MAX ]
+			then
+				MAX=$POTENTIAL
+				MAXACC=$ACC
+			fi
+		done
+		HASHMAXACC["$MAXACC"]="$MAX"
+		MAX=0
+		HASHACC["$MAXACC"]=0
+		((COUNTER++))
+	done
+
+	for ACC in "${!HASHMAXACC[@]}"; do echo "$ACC - ${HASHMAXACC[$ACC]}"; done
+
+}
+
+
+
+
