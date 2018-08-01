24e4dbb72d778a32d63293cf65eca5d603b8f2af
Yarden <yarden2k3@gmail.com>
2018-08-01 20:06:06 +0300

    user_validator function added

--- bash/ripple_score.sh
+++ bash/ripple_score.sh
@@ -27,19 +27,59 @@ function transfer_points {
 	FRSTPLAYER=$1
 	SCNDPLAYER=$2
 	SECRET=$3
-	
 	FPSEQ=`user_status $FRSTPLAYER`
 	FEE=10
 	SEQUENCE=`echo $FPSEQ |  grep "Sequence\"\s*:\s*[0-9]*" -o | sed 's/Sequence" : //'`
 	TRANSACTION='{"TransactionType": "Payment", "Account": "'$FRSTPLAYER'", "Destination": "'$SCNDPLAYER'", "Amount": "5000000",  "Fee": "'$FEE'", "Flags": "0", "Sequence": "'$SEQUENCE'"}'
 	SIGN=`rippled sign $SECRET "$TRANSACTION" offline 2>/dev/null`
 	TXBLOB=`echo $SIGN | grep "tx_blob\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/tx_blob" : "//'`
-	rippled submit $TXBLOB 2>/dev/null >/dev/null
+	rippled submit $TXBLOB #2>/dev/null >/dev/null
 }
 
 function user_status {
 	echo `rippled account_info $1 2>/dev/null`
 
+}
+
+function user_validator {
+	EXIT_CODE=1
+	ACCOUNT=$1
+	VALID="NOT OK"
+	ACC=`user_status $ACCOUNT`
+	BALANCE=`echo $ACC | grep "Balance\"\s*:\s*\"[0-9]*" -o | sed 's/Balance" : "//'`
+	if [ -z "$BALANCE" ]
+	then
+		VALID="Not Valid username or password empty BALANCE"
+	else
+		if [ $BALANCE -le 0 ]
+		then 
+			VALID="Not Valid username or password"
+		else
+			ACC=`user_status $ACCOUNT`
+			SEQUENCE=`echo $ACC |  grep "Sequence\"\s*:\s*[0-9]*" -o | sed 's/Sequence" : //'`
+			SECRET=$2
+			FEE=10
+			REGWALLET=`rippled wallet_propose 2>/dev/null`
+			REGACC=`echo $REGWALLET |  grep "account_id\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/account_id" : "//'`
+			TRANSACTION='{"TransactionType": "SetRegularKey", "Account": "'$ACCOUNT'", "RegularKey": "'$REGACC'",  "Fee": "'$FEE'", "Sequence": "'$SEQUENCE'"}'
+			SIGN=`rippled sign $SECRET "$TRANSACTION" offline 2>/dev/null`
+			TXBLOB=`echo $SIGN | grep "tx_blob\"\s*:\s*\"[a-zA-Z0-9]*" -o | sed 's/tx_blob" : "//'`
+			rippled submit $TXBLOB 2>/dev/null >/dev/null
+			ACC=`user_status $ACCOUNT`
+			
+			BALANCE2=`echo $ACC | grep "Balance\"\s*:\s*\"[0-9]*" -o | sed 's/Balance" : "//'`
+			if [ $BALANCE -eq $BALANCE2 ]
+			then
+				VALID="Balance equal Not Valid username or password"
+			else
+				VALID="OK"
+                                EXIT_CODE=0
+			fi
+		fi
+	fi
+	echo $VALID
+        exit $EXIT_CODE
+	
 }
 
 function top_ten {
@@ -81,8 +121,9 @@ function top_ten {
 		HASHACC["$MAXACC"]=0
 		((COUNTER++))
 	done
+	ANSWER=" "
+	for ACC in "${!HASHMAXACC[@]}"; do ANSWER+="Account ID : $ACC Score: ${HASHMAXACC[$ACC]}\n"; done
 
-	for ACC in "${!HASHMAXACC[@]}"; do echo "$ACC - ${HASHMAXACC[$ACC]}"; done
-
+	echo -e $ANSWER
 }
 
